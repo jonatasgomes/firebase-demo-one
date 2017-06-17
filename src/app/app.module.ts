6f5d5ca16369496b30dbbedfe8ec60360e9a01bb
Jônatas Gomes Alves <jonatasgomes@gmail.com>
2017-06-16 23:19:12 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTd3sdtzKRY-HqmoegdDG5mNu5v6RIhio",
+  authDomain: "what-we-do-923bf.firebaseapp.com",
+  databaseURL: "https://what-we-do-923bf.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "362901873794"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
